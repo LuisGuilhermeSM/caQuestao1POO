82d23aae11c4018e15350c18758f90913a7e0d98
Luís <lgsm4769@gmail.com>
2024-02-19 18:07:55 -0300

    Adicionar arquivos de projeto.

---
+++ caQuestao1/NomeProprio.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Policy;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace caQuestao1
+{
+    internal class NomeProprio
+    {
+        string nome_completo;
+        string nome_paper;
+
+        public string Nome_completo { get => nome_completo; set => nome_completo = value; }
+        public string Nome_paper { get => nome_paper; set => nome_paper = value; }
+
+        public NomeProprio()
+        {
+
+        }
+
+        public NomeProprio(string nome_completo)
+        {
+            Nome_completo = nome_completo;
+
+            string primeiro_nome = "";
+            string segundo_nome = "";
+            string sobrenome = "";
+            int qntEspaco = 0;
+
+            for(int i = 0; i < nome_completo.Length; i++)
+            {
+
+                if (qntEspaco == 0)
+                {
+                    //primeiro_nome = primeiro_nome + Convert.ToString(nome_completo[i]);
+                    if (Convert.ToString(nome_completo[i]) == " ")
+                    {
+                        qntEspaco = 1;
+                    }
+                    else
+                    {
+                        primeiro_nome = primeiro_nome + Convert.ToString(nome_completo[i]);
+                    }
+                }
+                else if (qntEspaco == 1)
+                {
+                    //segundo_nome = segundo_nome + Convert.ToString(nome_completo[i]);
+                    //Console.WriteLine("Entrou no segundo if");
+                    if (Convert.ToString(nome_completo[i]) == " ")
+                    {
+                        qntEspaco = 2;
+                    }
+                    else
+                    {
+                        segundo_nome = segundo_nome + Convert.ToString(nome_completo[i]);
+                    }
+                }
+                else if (qntEspaco == 2)
+                {
+                    //sobrenome = sobrenome + Convert.ToString(nome_completo[i]);
+                    if (Convert.ToString(nome_completo[i]) == " ")
+                    {
+                        qntEspaco = 2;
+                    }
+                    else
+                    {
+                        sobrenome = sobrenome + Convert.ToString(nome_completo[i]);
+                    }
+                }
+
+
+                //primeiro_nome = primeiro_nome + Convert.ToString(nome_completo[i]);
+            }
+
+            //Console.WriteLine(primeiro_nome);
+            //Console.WriteLine(segundo_nome);
+            //Console.WriteLine(sobrenome);
+            //Console.ReadLine();
+
+            string segundo_nome_paper = "";
+
+            if(qntEspaco == 2)
+            {
+                segundo_nome_paper = segundo_nome[0] + ".";
+                Nome_paper = sobrenome + ", " + primeiro_nome + " " + segundo_nome_paper; 
+            }
+            else if (qntEspaco == 1)
+            {
+                sobrenome = segundo_nome;
+                Nome_paper = sobrenome + ", " + primeiro_nome;
+            }
+
+            //Console.WriteLine(Nome_paper);
+            //Console.ReadLine();
+
+        }
+
+        public void ImprimeNomePaper()
+        {
+            Console.WriteLine(Nome_paper);
+            Console.ReadLine();
+        }
+
+    }
+}
---
+++ caQuestao1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace caQuestao1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Nome do aluno: Luís Guilherme Silva Matos
+
+            NomeProprio nome1 = new NomeProprio("Edgar Afonso Lamounier");
+            NomeProprio nome2 = new NomeProprio("Edgar A. Lamounier");
+            NomeProprio nome3 = new NomeProprio("Edgar Lamounier");
+
+            nome1.ImprimeNomePaper();
+            nome2.ImprimeNomePaper();
+            nome3.ImprimeNomePaper();
+        }
+    }
+}
